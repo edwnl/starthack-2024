06e54c4279146724cb37125c1f3ef427965fa611
Ed <edwinli123456@gmail.com>
2024-07-01 11:44:13 +1000

    chore: create-next-app and removed template code

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+      <div className="text-5xl justify-center text-center">
+        Hello there.
+      </div>
+  );
+}
