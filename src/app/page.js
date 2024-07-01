f160eb480a143567875e7e6207edffad2103edb2
Ed <edwinli123456@gmail.com>
2024-07-01 12:02:31 +1000

    chore: installed prettier

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,11 @@ import {Button} from "antd";
 
 export default function Home() {
   return (
-      <>
-          <div className="text-center flex-row">
-              <div className="text-5xl ">
-                  Hello there.
-              </div>
-              <Button>This is a button from antd!</Button>
+      <div className="text-center flex-row">
+          <div className="text-5xl ">
+              Hello there.
           </div>
-      </>
+          <Button>This is a button from antd!</Button>
+      </div>
   );
 }
