d0408928756903816ea0fac69c99ebd70a5287f0
PhiPhiPham <Stevenpham0705@gmail.com>
2024-07-06 23:47:17 +1000

    Added the route to the results page

--- src/components/LargeSearchBar.js
+++ src/components/LargeSearchBar.js
@@ -1,8 +1,18 @@
+"use client";
+
 import React from "react";
 import { Input, Button } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
 
 const LargeSearchBar = () => {
+  const router = useRouter();
+
+  const handleSearch = () => {
+    // Perform any necessary search logic or state updates
+    router.push("/results");
+  };
+
   return (
     <div className="flex items-center justify-center py-2 px-4 border rounded-xl w-full">
       <Input
@@ -14,6 +24,7 @@ const LargeSearchBar = () => {
         type="primary"
         icon={<SearchOutlined />}
         className="rounded-xl px-3 py-5 text-lg flex items-center"
+        onClick={handleSearch}
       >
         Find Groups
       </Button>
