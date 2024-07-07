8b6d965e22436270a1ae25a778855c98da9d1c9c
Will Keeble <will.a.keeble@gmail.com>
2024-07-07 23:06:57 +1000

    Add Leaderboard frontend

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,7 +1,63 @@
+"use client";
+
 import React from "react";
+import { Table, Tabs } from "antd";
+import {
+  allTimeData,
+  columns,
+  monthlyData,
+  weeklyData,
+  yearlyData,
+} from "./fakeData";
+import Title from "antd/es/typography/Title";
+
+const { TabPane } = Tabs;
 
 const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  return (
+    <div>
+      <Title
+        level={2}
+        style={{
+          fontSize: "48px",
+          fontWeight: "500",
+          marginBottom: "0.5rem",
+        }}
+      >
+        Leaderboard
+      </Title>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Weekly" key="1">
+          <Table
+            dataSource={weeklyData}
+            columns={columns}
+            pagination={{ pageSize: 10 }}
+          />
+        </TabPane>
+        <TabPane tab="Monthly" key="2">
+          <Table
+            dataSource={monthlyData}
+            columns={columns}
+            pagination={{ pageSize: 10 }}
+          />
+        </TabPane>
+        <TabPane tab="Yearly" key="3">
+          <Table
+            dataSource={yearlyData}
+            columns={columns}
+            pagination={{ pageSize: 10 }}
+          />
+        </TabPane>
+        <TabPane tab="All-Time" key="4">
+          <Table
+            dataSource={allTimeData}
+            columns={columns}
+            pagination={{ pageSize: 10 }}
+          />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Leaderboard;
